3802c3aa1b52d993388d75e430422a3987138008
noobapoo99 <apoorvtripathi99@gmail.com>
2024-12-06 18:38:49 +0530

    updated app

--- app.js
+++ app.js
@@ -7,13 +7,27 @@ import testRoute from "./routes/test.route.js";
 import userRoute from "./routes/user.route.js";
 import chatRoute from "./routes/chat.route.js";
 import messageRoute from "./routes/message.route.js";
+import dotenv from "dotenv";
+
+// Load environment variables
+dotenv.config();
 
 const app = express();
 
-app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+// CORS configuration
+const corsOptions = {
+  origin: "https://estate-frontend.netlify.app", // Frontend URL
+  credentials: true, // Allow cookies
+  methods: ["GET", "POST", "PUT", "DELETE"], // Specify allowed HTTP methods
+  allowedHeaders: ["Content-Type", "Authorization"], // Specify allowed headers
+};
+
+app.use(cors(corsOptions)); // Enable CORS with options
+
 app.use(express.json());
 app.use(cookieParser());
 
+// API routes
 app.use("/api/posts", postRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/test", testRoute);
